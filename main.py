d79d4cf15e7ce04329d257b2a8df82b6b1ccd7ff
WeaverDyl <dweaver@umd.edu>
2020-01-16 20:54:46 -0500

    some basic implementation complete

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
-def get_user_list():
-    # Get all users in C:\Program Files (x86)\Steam\userdata\
-    # Make it work os-agnostically
-    pass
+from pathlib import Path
+from steamfiles import appinfo
 
-def get_sharedconfig(user):
+def get_user_list():
+    # Gets a list of users found on the machine
+    users = []
+
+    # Make work with other OS's
+    path = Path('C:\\Program Files (x86)\\Steam\\userdata')
+    
+    # Add each directory name to the list of users
+    for user_id in path.iterdir():
+        if user_id.is_dir():
+            users.append(user_id.name)
+
+    return users
+
+def ask_user_id(users):
+    # Ask the user which user to select from the get_user_list() func
+    chosen_user = None # Set up
+    user_map = {} # Stores user_id as value and option_id as key
+    option_id = 1
+
+    # Assign each user a unique option_id
+    for user in users:
+        user_map[str(option_id)] = user
+        option_id += 1
+    
+    # Ask user for selection, if selection not valid, ask again
+    while chosen_user not in list(user_map.values()):
+        print("Please choose a user:")
+        
+        for user, key_mapping in user_map.items():
+            print(f"{user}. '{key_mapping}'")
+
+        chosen_key_mapping = input() # Allow user input
+        
+        try:
+            chosen_user = user_map[chosen_key_mapping]
+        except KeyError:
+            continue
+
+    return chosen_user
+
+def get_sharedconfig(user_id):
     # Given a user, return the sharedconfig file located at
     # \user\7\remote\sharedconfig.vdf
 
@@ -13,10 +52,6 @@ def get_sharedconfig(user):
 
 def get_collections(sharedconfig):
     # Get a list of all of the collections in the sharedconfig file
-    pass
-
-def ask_user_id(users):
-    # Ask the user which user to select from the get_user_list() func
     pass
 
 def ask_user_collection(collections):
@@ -30,14 +65,14 @@ def choose_game(collection):
 
 
 all_users = get_user_list() # All users
-chosen_user = ask_user_id() # Ask which user to select a game for
+chosen_user = ask_user_id(all_users) # Ask which user to select a game for
 
-sharedconfig = get_sharedconfig(chosen_user)
+# sharedconfig = get_sharedconfig(chosen_user) # Reference to sharedconfig
 
-all_collections = get_collections(sharedconfig)
-chosen_collection = ask_user_collection(all_collections)
+# all_collections = get_collections(sharedconfig) # List of all game collections
+# chosen_collection = ask_user_collection(all_collections) # The selected collection
 
-choose_game(chosen_collection)
+# choose_game(chosen_collection) # Pick a random game and tell the user what it is
 
 # TODO
 # - can easily just make a function to choose from a python list
